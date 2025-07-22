449feb61084dc350bfce472198df220d4f7e617b
Necromunger <lil_tommy_tom@hotmail.com>
2025-07-22 19:07:36 +1000

    serverside harmony patch block providing player map layer data

--- HidePlayerInfo/HidePlayerInfoModSystem.cs
+++ HidePlayerInfo/HidePlayerInfoModSystem.cs
@@ -1,5 +1,7 @@
-﻿using Vintagestory.API.Client;
+﻿using HarmonyLib;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Server;
 using Vintagestory.GameContent;
 
 namespace HidePlayerInfo;
@@ -8,12 +10,24 @@ public class HidePlayerInfoModSystem : ModSystem
 {
     public override void StartClientSide(ICoreClientAPI api)
     {
-        api.Event.LevelFinalize += () =>
-        {
-            var map = api.ModLoader.GetModSystem<WorldMapManager>();
-            map.MapLayerRegistry.Remove("players");
-            map.LayerGroupPositions.Remove("players");
-            map.MapLayers.RemoveAll(l => l is PlayerMapLayer);
-        };
+        api.World.Config.SetBool("mapHideOtherPlayers", true);
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        var harmony = new Harmony("necromunger.hideplayerinfo");
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(WorldMapManager), nameof(WorldMapManager.SendMapDataToClient))]
+class Patch_SendMapDataToClient
+{
+    static bool Prefix(WorldMapManager map, MapLayer forMapLayer, IServerPlayer forPlayer, byte[] data)
+    {
+        if (forMapLayer is PlayerMapLayer)
+            return false;
+
+        return true;
     }
 }
